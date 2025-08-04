d496c4a5c67a06afff14433034e341ff45d8c890
jeferson <jeferson@escape.ppg.br>
2025-08-04 18:01:51 -0300

    criação do carrinho.

---
+++ src/components/cart/cart-list.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useCart } from "@/stores/cart";
+import { Button } from "../ui/button";
+import { useProducts } from "@/stores/products";
+import { useEffect, useState } from "react";
+import { CartProduct } from "./cart-product";
+import { decimalToMoney } from "@/lib/utils";
+
+export const CartList = () => {
+  const cart = useCart();
+  const products = useProducts();
+
+  const [subtotal, setSubTotal] = useState(0);
+  const [shippingCost, setShippingCost] = useState(10);
+
+  const calculateSubtotal = () => {
+    let sub = 0;
+    for (let item of cart.items) {
+      const prod = products.products.find(
+        (pitem) => pitem.id === item.productId
+      );
+      if (prod) sub += item.quantity * parseFloat(prod.price.toString());
+    }
+
+    setSubTotal(sub);
+  };
+
+  useEffect(calculateSubtotal, [cart]);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-3 my-5">
+        {cart.items.map((item) => (
+          <CartProduct key={item.productId} data={item} />
+        ))}
+      </div>
+      <div className="my-4 text-right">
+        <div>Sub-total: {decimalToMoney(subtotal)}</div>
+        <div>Frete: {decimalToMoney(shippingCost)}</div>
+        <div className="font-bold">
+          Total{decimalToMoney(subtotal + shippingCost)}
+        </div>
+      </div>
+      <Button>Finalizar compra</Button>
+    </div>
+  );
+};
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,6 @@
-"use client";
-
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { CartButton } from "../cart/cart-button";
 
 export const Header = () => {
   return (
@@ -12,7 +11,7 @@ export const Header = () => {
 
       <div className="flex gap-2">
         <Button>Login / Cadastro</Button>
-        <Button>Carrinho</Button>
+        <CartButton />
       </div>
     </header>
   );

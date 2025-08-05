5eaee7e235712fb10454460d52e845a378445167
jeferson <jeferson@escape.ppg.br>
2025-08-05 17:56:56 -0300

    API de autenticação: Services

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -14,12 +14,36 @@ export const validateAuth = async (email: string, password: string) => {
     where: { email },
   });
   if (!user) return false;
+  if (!bcrypt.compareSync(password, user.password)) return false;
+
+  return {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  };
 };
 
 export const createUser = async (
   name: string,
   email: string,
   password: string
-) => {};
+) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email: email.toLowerCase(),
+        password: bcrypt.hashSync(password, 10),
+      },
+    });
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+  } catch (err) {
+    return null;
+  }
+};
 
 export const createUserToken = async (userId: number) => {};
